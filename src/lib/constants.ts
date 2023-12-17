5d37de7179b03a19c7b5b4e367139dc1b8106201
Kevin Muñoz <kmunoz@condorbs.net>
2023-12-17 14:09:59 -0500

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const LINKS = {
+  github: "https://github.com/TheSL18",
+  linkedin: "https://www.linkedin.com/in/kevin-mu%C3%B1oz-289830274/",
+  //medium: "https://ladvace.medium.com/",
+  //discord: "https://discordapp.com/users/163300027618295808",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
